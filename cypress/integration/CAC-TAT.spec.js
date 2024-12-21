b1afe534f00e3450c1267377b3c4656daaa287ab
jacksontestercode <jacksonfla6@gmail.com>
2024-12-21 13:21:53 -0300

    Atalização

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -4,7 +4,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it.only('verifica o título da aplicação', function() {
         cy.visit('./src/index.html')
 
-        cy.title().should('Central de Atendimento ao Cliente TAT')
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
     
   
     })
