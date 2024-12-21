b4004fec38414c7d2ddd702f799f74e5f9b82664
jacksontestercode <jacksonfla6@gmail.com>
2024-12-21 13:20:43 -0300

    atualizaçao

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,11 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
+    it.only('verifica o título da aplicação', function() {
         cy.visit('./src/index.html')
 
-        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.title().should('Central de Atendimento ao Cliente TAT')
+    
   
     })
   })
